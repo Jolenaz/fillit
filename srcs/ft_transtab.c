a5e27a9021456aa6c5b1fd5f5a5446261676b2e7
Jonas BELLESSA <jbelless@e3r11p5.42.fr>
2015-12-11 17:43:12 +0100

    creation de la fonction qui transforme le tableu de piece en fonction de size

---
+++ srcs/ft_transtab.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_transtab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbelless <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/11 13:32:46 by jbelless          #+#    #+#             */
+/*   Updated: 2015/12/11 17:41:13 by jbelless         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static void	ft_init(int **tab)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 27)
+	{
+		j = 0;
+		while (j < 5)
+		{
+			tab[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+}
+
+static int	**ft_intnewtab(void)
+{
+	int i;
+	int **res;
+
+	i = 0;
+	res = NULL;
+	if ((res = (int**)malloc(sizeof(int*) * 27)) == NULL)
+		return (NULL);
+	while (i < 27)
+	{
+		if ((res[i] = (int*)malloc(sizeof(int) * 5)) == NULL)
+			return (NULL);
+		i++;
+	}
+	ft_init(res);
+	return (res);
+}
+
+int			**ft_transtab(int **tab, size_t s)
+{
+	int **res;
+	int	i;
+	int	j;
+	int k;
+
+	i = 0;
+	if ((res = ft_intnewtab()) == NULL)
+		return (NULL);
+	while (i < 27)
+	{
+		res[i][0] = tab[i][0];
+		k = 0;
+		j = 1;
+		while (++j < 5)
+		{
+			if (tab[i][j] == 0)
+				res[i][0] = -1;
+			else if (tab[i][j] == tab[i][j - 1] + 1)
+				res[i][j] = tab[i][j] % 4 + k * s;
+			else
+				res[i][j] = tab[i][j] % 4 + ++k * s;
+		}
+		i++;
+	}
+	return (res);
+}
