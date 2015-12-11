fe90d26ff2adc6fa7c8fa3315c5a7f974595fa3e
Alexandre GASPAR <agaspar@e3r10p7.42.fr>
2015-12-11 18:04:15 +0100

    Modification mineur

--- srcs/read_file.c
+++ srcs/read_file.c
@@ -6,7 +6,7 @@
 /*   By: agaspar <agaspar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/01 17:01:49 by agaspar           #+#    #+#             */
-/*   Updated: 2015/12/11 10:09:01 by jbelless         ###   ########.fr       */
+/*   Updated: 2015/12/11 17:24:48 by agaspar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,62 @@
 #include <tetriminos.h>
 #include <stdio.h>
 
+static int	**create_tab(char *buf)
+{
+	int		**tabpd;
+	int		i;
+	int		k;
+	int		j;
+	int		*tmp;
+
+	k = 0;
+	i = 0;
+	tabpd = malloc(sizeof(int*) * 27);
+	if (tabpd == NULL)
+		return (NULL);
+	while (buf[i])
+	{
+		tabpd[k] = (int*)malloc(sizeof(int) * 5);
+		if (!tabpd[k])
+			return (NULL);
+		tmp = tabpd[k];
+		*tmp++ = i / 21;
+		j = 0;
+		while (j < 20)
+		{
+			if (buf[i] == '#')
+				*tmp++ = j;
+			j++;
+			i++;
+		}
+		i++;
+		k++;
+	}
+	i = 0;
+	while (i < 8)
+	{
+		ft_putstr("tab n: ");
+		ft_putnbr(i);
+		ft_putchar('\n');
+		k = 0;
+		while (k < 5)
+		{
+
+			ft_putnbr(tabpd[i][k]);
+			ft_putstr(", ");
+			k++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+	return (tabpd);
+}
+
 static char	*read_file(int fd)
 {
 	char	*buf;
 	int		i;
+	int		**tabpd;
 
 	i = 0;
 	buf = ft_strnew(BUF_SIZE);
@@ -28,7 +80,9 @@ static char	*read_file(int fd)
 		if (read(fd, buf, BUF_SIZE) != -1)
 		{
 			if (check_tetr(buf))
-				ft_putstr(buf);
+				tabpd = create_tab(buf);
+			else
+				ft_putendl("Error");
 			i++;
 		}
 		ft_putstr("Tetriminos loaded :");
