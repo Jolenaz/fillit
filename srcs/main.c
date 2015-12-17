c9c396e8ad9c9a1a24ea5eeb995189ee6fa200a6
Alexandre GASPAR <agaspar@e3r10p6.42.fr>
2015-12-17 14:15:13 +0100

    Mise a la norme

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: agaspar <agaspar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/01 13:24:38 by agaspar           #+#    #+#             */
-/*   Updated: 2015/12/17 14:04:45 by agaspar          ###   ########.fr       */
+/*   Updated: 2015/12/17 14:15:02 by agaspar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ int		main(int argc, char **argv)
 	if (argc != 2)
 		error("Fillit require juste one argument !");
 	else
+	{
 		if (!load_file(argv[1]))
 			error("Probleme au chargement du fichier");
+	}
 	return (1);
 }
